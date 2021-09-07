f6d238b80b9ce26d819c3d55a1d129e9ec37e49a
Matthew Pachapa <mpachapa@uncc.edu>
2021-09-07 10:59:53 -0400

    test

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,10 @@
+#include <unistd.h>
+#include <iostream>
 int main () {
-
+  //long host = gethostid(void);
+  std::string s = "hello world\n";
+  std::cout<<s;
+  //std::cout<<"host name: "<<host<<"\n";
+  
   return 0;
 }
