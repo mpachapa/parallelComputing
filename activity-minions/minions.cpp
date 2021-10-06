121177dc6b9a159e3e39905fb7688bb1108b7103
Matthew Pachapa <mpachapa@uncc.edu>
2021-10-06 14:53:48 -0400

    minions

--- activity-minions/minions.cpp
+++ activity-minions/minions.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <thread>
+
+//minion thread call
+void minion(int i){
+ 	std::cout<<"Hello! I am Minion "<<i<<"!\n";
+}
+//overlord thread call
+void overlord(){
+  std::cout<<"Hello Minions! I am the Overlord!\n";
+}
 
 
 int main (int argc, char** argv) {
@@ -6,7 +16,17 @@ int main (int argc, char** argv) {
     std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
     return -1;
   }
-  //Your code goes here
+//extracting the passed value
+  int numOf = atoi(argv[1]);
+  
+//initalization of the minion thread
+    for (int i = 1; i<=numOf; i++){
+	std::thread t1(minion, i);  
+  	t1.join();
+  }
+//initalization of the overlord thread
+  std::thread t3(overlord);
+  t3.join();
   
   return 0;
 }
