93d597c38e62ed1cf5e7ec9c7233e9274b489a44
Matthew Pachapa <mpachapa@uncc.edu>
2021-12-09 13:40:45 -0500

    MPI PP

--- assignment-mpi-ping-pong/pingpong/mpi_ping_pong.cpp
+++ assignment-mpi-ping-pong/pingpong/mpi_ping_pong.cpp
@@ -7,9 +7,25 @@ int main (int argc, char* argv[]) {
     std::cerr<<"usage: mpirun "<<argv[0]<<" <value>"<<std::endl;
     return -1;
   }
+	// Find out rank, size
+	int worldrank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &worldrank);
+	int worldsize;
+	MPI_Comm_size(MPI_COMM_WORLD, &worldsize);
 
-  
+	int number;
+	if (worldrank == 0) {
+    		number = atoi(argv[1)]);
+    		MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	} else if (worldrank == 1) {
+    		MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		number +=2;
+		MPI_Send(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    	}
+	MPI_Recv(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  	std::cout<<number;
 
+	MPI_Finalize();
 
   return 0;
 }
