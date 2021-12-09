f81192ef4e14926c74e4926aa56a6f3253923d44
Matthew Pachapa <mpachapa@uncc.edu>
2021-12-09 12:24:27 -0500

    MPI hi

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -3,5 +3,19 @@
 #include <iostream>
 
 int main(int argc, char*argv[]) {
+	char hostname[256];
+  	hostname[255] = '\0';
+  	gethostname(hostname, 255);
+	
+	MPI_Init(&argc,&argv);
 
+	int i;
+	int P;
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &P);
+	MPI_Comm_rank(MPI_COMM_WORLD, &i);
+		std::cout<<"I am process "<< i <<" out of "<< P <<". I am running on machine: "<< hostname <<"\n";
+
+
+	MPI_Finalize();
 }
